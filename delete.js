b5fdb9cb19623fbc6c2590b83420b47619efde28
Dharmen Shah <dharmen.s@milestoneinternet.com>
2020-10-30 15:55:05 +0530

    chore(release): release fixes

--- delete.js
+++ delete.js
@@ -5,6 +5,7 @@ const del = require('del');
     'src/copy/**',
     '!src/copy',
     '!src/copy/index.html',
+    '!src/copy/logo.png',
   ]);
 
   console.log('Deleted files:\n', deletedFilePaths.join('\n'));
